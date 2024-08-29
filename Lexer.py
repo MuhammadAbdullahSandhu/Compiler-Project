634a4a097917e0014c1d38f857dda8157f201acc
MuhammadAbdullahSandhu <157258298+MuhammadAbdullahSandhu@users.noreply.github.com>
2024-08-28 22:16:34 -0500

    change

---
+++ Lexer.py
@@ -0,0 +1,7 @@
+string = "Muhammad Abdullah"
+index = string.find('A')
+
+if index != -1:
+    print("The character 'A' is at: ", index)
+else:
+    print('Not present')
---
+++ test.py
@@ -0,0 +1,4 @@
+c = 4 + True # 5
+d = True
+if d == 1:
+    print('d is False')
