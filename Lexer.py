930153a3eaa5d67f3e138a33dcf8e00f5a07eefc
MuhammadAbdullahSandhu <157258298+MuhammadAbdullahSandhu@users.noreply.github.com>
2024-08-28 22:20:41 -0500

    Update Lexer.py

--- Lexer.py
+++ Lexer.py
@@ -1,6 +1,7 @@
 string = "Muhammad Abdullah"
-index = string.find('A')
 
+text = input("Enter Alphabet")
+index = string.find(text)
 if index != -1:
     print("The character 'A' is at: ", index)
 else:
